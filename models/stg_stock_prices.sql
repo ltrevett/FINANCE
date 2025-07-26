e927ef3b653c33457b0fa40c6b86f3e146f3932c
lucastrevett12@outlook.com <lucastrevett12@outlook.com>
2025-07-26 13:57:34 +0000

    Added stock prices stage

---
+++ models/stg_stock_prices.sql
@@ -0,0 +1,13 @@
+
+select 
+
+    ticker,
+    asset_class,
+    primary_exchange_code,
+    primary_exchange_name,
+    variable,
+    variable_name,
+    date,
+    value
+
+from {{ source('FINANCE__ECONOMICS', 'STOCK_PRICE_TIMESERIES') }}
